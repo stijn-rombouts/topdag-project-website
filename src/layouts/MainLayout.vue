37b5ac88bacd82b4f0fad502292d753305f82afe
stijn-rombouts <r1036855@student.thomasmore.be>
2025-04-29 19:22:48 +0200

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <!-- <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          TopDag Project
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header> -->
+
+    <!-- <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in linksList"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer> -->
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="flex flex-center bg-dark">
+    <div class="column items-center">
+      <img
+        alt="FutureLens Logo"
+        src="~assets/FutureLensLogoFull.png"
+        style="width: 200px; height: 200px"
+      />
+      <q-separator spaced inset vertical dark />
+      <q-btn
+        outline
+        rounded
+        icon="download"
+        color="orange"
+        class="text-weight-bolder"
+        label="Download"
+        size="xl"
+        @click="download"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+function download() {
+  const link = document.createElement('a')
+  link.href =
+    'https://github.com/stijn-rombouts/topdag-project-website/releases/download/0.1/test10.apk'
+  link.download = 'test10.apk'
+  link.click()
+}
+</script>
